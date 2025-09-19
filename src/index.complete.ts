ff1cfea9b5de3b2e52c4824833cb37be71bf46c7
Sorami Shiromizu <hisamoto@mierune.co.jp>
2025-09-19 16:02:21 +0900

    chore: remove temporary .complete files
    These were reference files for the full implementation and shouldn't be in the PR.

--- src/index.complete.ts
+++
@@ -1,7 +0,0 @@
-// Entry point for the library
-export { MockGeolocateControl } from "./MockGeolocateControl";
-export type {
-  MockGeolocateControlOptions,
-  GeolocateEventData,
-  OutOfMaxBoundsEventData,
-} from "./types";
