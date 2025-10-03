414bd81fffe09e6d5acb2571538152927c7ac7b4
Sorami Shiromizu <hisamoto@mierune.co.jp>
2025-10-03 12:36:56 +0900

    Inline bearing default in fitBounds call

--- src/MockGeolocateControl.ts
+++ src/MockGeolocateControl.ts
@@ -275,16 +275,11 @@ export class MockGeolocateControl implements IControl {
     // Create bounds from position and accuracy radius
     const bounds = LngLatBounds.fromLngLat(this._position, this._accuracy);
 
-    // Fit the map to the bounds while keeping the current bearing unless overridden
-    const fitBoundsOptions: FitBoundsOptions & { bearing?: number } = {
+    this._map.fitBounds(bounds, {
       ...this._fitBoundsOptions,
-    };
-
-    if (fitBoundsOptions.bearing === undefined) {
-      fitBoundsOptions.bearing = this._map.getBearing();
-    }
-
-    this._map.fitBounds(bounds, fitBoundsOptions);
+      bearing:
+        this._fitBoundsOptions.bearing ?? this._map.getBearing(),
+    });
   }
 
   /**
